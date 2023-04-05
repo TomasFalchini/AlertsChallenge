01377bca95606af255e317aca8c5321fca106f62
Tomas Falchini <tomasfalchini@gmail.com>
2023-04-05 19:34:37 -0300

    feat(alerts.repository.ts): add sendTopicAlertToAllSubscribers and sendTopicAlertToUser methods to AlertManager class

--- src/3. Infrastructure/Repositorie/alerts.repository.ts
+++ src/3. Infrastructure/Repositorie/alerts.repository.ts
@@ -1,4 +1,12 @@
 import { AlertEntity } from "../../1. Domain/alert.entity";
 import { AlertsRepository } from "../../1. Domain/alerts.repository";
 
-class AlertManager implements AlertsRepository {}
+class AlertManager implements AlertsRepository {
+  sendTopicAlertToAllSubscribers(topic: string, alert: AlertEntity): void {}
+
+  sendTopicAlertToUser(
+    topic: string,
+    alert: AlertEntity,
+    userId: number
+  ): void {}
+}
